97efb367cb266809d0476873754637dc9e34f574
kristelle01 <kristellebenedicto@gmail.com>
2025-04-26 00:19:00 +0800

    front end

---
+++ Dashboard.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace elemStudentInfo
+{
+    partial class Dashboard
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.studentInfo = new System.Windows.Forms.Button();
+            this.student = new System.Windows.Forms.Button();
+            this.subject = new System.Windows.Forms.Button();
+            this.teacher = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Bisque;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1181, 75);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Bisque;
+            this.panel2.Controls.Add(this.teacher);
+            this.panel2.Controls.Add(this.subject);
+            this.panel2.Controls.Add(this.student);
+            this.panel2.Controls.Add(this.studentInfo);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel2.Location = new System.Drawing.Point(0, 75);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(205, 515);
+            this.panel2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Rockwell", 28.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.SaddleBrown;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(668, 57);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Student Information System";
+            // 
+            // studentInfo
+            // 
+            this.studentInfo.Location = new System.Drawing.Point(16, 21);
+            this.studentInfo.Name = "studentInfo";
+            this.studentInfo.Size = new System.Drawing.Size(173, 51);
+            this.studentInfo.TabIndex = 0;
+            this.studentInfo.Text = "Student Information";
+            this.studentInfo.UseVisualStyleBackColor = true;
+            // 
+            // student
+            // 
+            this.student.Location = new System.Drawing.Point(16, 88);
+            this.student.Name = "student";
+            this.student.Size = new System.Drawing.Size(173, 51);
+            this.student.TabIndex = 1;
+            this.student.Text = "Student";
+            this.student.UseVisualStyleBackColor = true;
+            // 
+            // subject
+            // 
+            this.subject.Location = new System.Drawing.Point(16, 154);
+            this.subject.Name = "subject";
+            this.subject.Size = new System.Drawing.Size(173, 51);
+            this.subject.TabIndex = 2;
+            this.subject.Text = "Subject";
+            this.subject.UseVisualStyleBackColor = true;
+            // 
+            // teacher
+            // 
+            this.teacher.Location = new System.Drawing.Point(16, 221);
+            this.teacher.Name = "teacher";
+            this.teacher.Size = new System.Drawing.Size(173, 51);
+            this.teacher.TabIndex = 3;
+            this.teacher.Text = "Teacher";
+            this.teacher.UseVisualStyleBackColor = true;
+            // 
+            // Dashboard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::elemStudentInfo.Properties.Resources.Untitled_design__2_;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1181, 590);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Dashboard";
+            this.Text = "Dashboard";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button studentInfo;
+        private System.Windows.Forms.Button teacher;
+        private System.Windows.Forms.Button subject;
+        private System.Windows.Forms.Button student;
+    }
+}
---
+++ LoginForm.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace elemStudentInfo
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.grouplogin = new System.Windows.Forms.GroupBox();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.loginTitle = new System.Windows.Forms.Label();
+            this.password = new System.Windows.Forms.TextBox();
+            this.pass = new System.Windows.Forms.Label();
+            this.frmusername = new System.Windows.Forms.TextBox();
+            this.username = new System.Windows.Forms.Label();
+            this.btnlogin = new System.Windows.Forms.Button();
+            this.showpass = new System.Windows.Forms.CheckBox();
+            this.grouplogin.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // grouplogin
+            // 
+            this.grouplogin.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.grouplogin.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.grouplogin.Controls.Add(this.showpass);
+            this.grouplogin.Controls.Add(this.btnCancel);
+            this.grouplogin.Controls.Add(this.loginTitle);
+            this.grouplogin.Controls.Add(this.password);
+            this.grouplogin.Controls.Add(this.pass);
+            this.grouplogin.Controls.Add(this.frmusername);
+            this.grouplogin.Controls.Add(this.username);
+            this.grouplogin.Controls.Add(this.btnlogin);
+            this.grouplogin.Location = new System.Drawing.Point(226, 85);
+            this.grouplogin.Name = "grouplogin";
+            this.grouplogin.Size = new System.Drawing.Size(743, 450);
+            this.grouplogin.TabIndex = 0;
+            this.grouplogin.TabStop = false;
+            this.grouplogin.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnCancel.BackColor = System.Drawing.Color.LightCoral;
+            this.btnCancel.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancel.Location = new System.Drawing.Point(101, 277);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(249, 51);
+            this.btnCancel.TabIndex = 6;
+            this.btnCancel.Text = "CANCEL";
+            this.btnCancel.UseVisualStyleBackColor = false;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // loginTitle
+            // 
+            this.loginTitle.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.loginTitle.AutoSize = true;
+            this.loginTitle.Font = new System.Drawing.Font("Stencil", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.loginTitle.Location = new System.Drawing.Point(26, 40);
+            this.loginTitle.Name = "loginTitle";
+            this.loginTitle.Size = new System.Drawing.Size(662, 33);
+            this.loginTitle.TabIndex = 5;
+            this.loginTitle.Text = "ELEMENTARY STUDENT INFORMATION SYSTEM";
+            // 
+            // password
+            // 
+            this.password.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.password.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.password.Location = new System.Drawing.Point(242, 180);
+            this.password.Name = "password";
+            this.password.Size = new System.Drawing.Size(388, 34);
+            this.password.TabIndex = 4;
+            // 
+            // pass
+            // 
+            this.pass.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.pass.AutoSize = true;
+            this.pass.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.pass.Location = new System.Drawing.Point(96, 183);
+            this.pass.Name = "pass";
+            this.pass.Size = new System.Drawing.Size(117, 25);
+            this.pass.TabIndex = 3;
+            this.pass.Text = "Password:";
+            // 
+            // frmusername
+            // 
+            this.frmusername.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.frmusername.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.frmusername.Location = new System.Drawing.Point(242, 124);
+            this.frmusername.Name = "frmusername";
+            this.frmusername.Size = new System.Drawing.Size(388, 34);
+            this.frmusername.TabIndex = 2;
+            this.frmusername.TextChanged += new System.EventHandler(this.textBox1_TextChanged_1);
+            // 
+            // username
+            // 
+            this.username.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.username.AutoSize = true;
+            this.username.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.username.Location = new System.Drawing.Point(96, 127);
+            this.username.Name = "username";
+            this.username.Size = new System.Drawing.Size(122, 25);
+            this.username.TabIndex = 1;
+            this.username.Text = "Username:";
+            this.username.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // btnlogin
+            // 
+            this.btnlogin.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnlogin.BackColor = System.Drawing.Color.YellowGreen;
+            this.btnlogin.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnlogin.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnlogin.Location = new System.Drawing.Point(381, 277);
+            this.btnlogin.Name = "btnlogin";
+            this.btnlogin.Size = new System.Drawing.Size(249, 51);
+            this.btnlogin.TabIndex = 0;
+            this.btnlogin.Text = "LOGIN";
+            this.btnlogin.UseVisualStyleBackColor = false;
+            this.btnlogin.Click += new System.EventHandler(this.btnlogin_Click);
+            // 
+            // showpass
+            // 
+            this.showpass.AutoSize = true;
+            this.showpass.Location = new System.Drawing.Point(249, 219);
+            this.showpass.Name = "showpass";
+            this.showpass.Size = new System.Drawing.Size(186, 20);
+            this.showpass.TabIndex = 7;
+            this.showpass.Text = "Show Password Cahracter";
+            this.showpass.UseVisualStyleBackColor = true;
+            this.showpass.CheckedChanged += new System.EventHandler(this.showpass_CheckedChanged);
+            // 
+            // LoginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::elemStudentInfo.Properties.Resources.Untitled_design__1_;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1181, 590);
+            this.Controls.Add(this.grouplogin);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "LoginForm";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.grouplogin.ResumeLayout(false);
+            this.grouplogin.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox grouplogin;
+        private System.Windows.Forms.Button btnlogin;
+        private System.Windows.Forms.Label username;
+        private System.Windows.Forms.TextBox frmusername;
+        private System.Windows.Forms.TextBox password;
+        private System.Windows.Forms.Label pass;
+        private System.Windows.Forms.Label loginTitle;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.CheckBox showpass;
+    }
+}
+
